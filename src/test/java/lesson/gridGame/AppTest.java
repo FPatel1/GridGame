b6e2689063051b75156c3df39fa17739709bf973
FPatel1 <fyakubpatel@gmail.com>
2022-11-02 16:33:11 +0000

    Added test that fails

--- src/test/java/lesson/gridGame/AppTest.java
+++ src/test/java/lesson/gridGame/AppTest.java
@@ -4,18 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
-
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void failTest() {
+		assertTrue(false);
+	}
 }
